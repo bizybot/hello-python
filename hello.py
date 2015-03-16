3b8774d2ebb0c047bc23011502e50a023ce02446
BizyBot <bizybot@gmail.com>
2015-03-16 23:51:41 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python
 
-print "hello python!"
+import json
+import requests
+import random
+import time
+while True:
+        temperature=random.randint(90,120)
+        print("temperature is "+str(temperature))
+        url = "https://dweet.io:443/dweet/for/iotmanager-temperature-sensor-1"
+        data = {'deviceId': 'Device-001', 'name': 'MasterRoom', 'temperature': temperature}
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+        time.sleep(5)
